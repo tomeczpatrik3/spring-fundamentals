01926eb286281bfed8e7274e6739874c4e964682
Patrik Tomecz <tomeczpatrik3@gmail.com>
2019-09-20 16:18:29 +0200

    Properties: XML cfg

--- spring_sample_xml/src/main/java/com/pluralsight/repository/HibernateCustomerRepositoryImpl.java
+++ spring_sample_xml/src/main/java/com/pluralsight/repository/HibernateCustomerRepositoryImpl.java
@@ -6,14 +6,22 @@ import java.util.List;
 import com.pluralsight.model.Customer;
 
 public class HibernateCustomerRepositoryImpl implements CustomerRepository {
-	
+
+	private String dbUsername;
+
+	public void setDbUsername(String dbUsername) {
+		this.dbUsername = dbUsername;
+	}
+
 	@Override
 	public List<Customer> findAll() {
+		System.out.println(this.dbUsername);
+		
 		Customer customer = new Customer();
 		customer.setFirstname("Patrik");
 		customer.setLastname("Tomecz");
-		
+
 		return Arrays.asList(customer);
 	}
-	
+
 }
