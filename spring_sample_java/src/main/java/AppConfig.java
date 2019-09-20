5855f73ef1ef8acb25354e79bb9d63ad11fc91c5
Patrik Tomecz <tomeczpatrik3@gmail.com>
2019-09-20 16:26:22 +0200

    Properties: Java cfg

--- spring_sample_java/src/main/java/AppConfig.java
+++ spring_sample_java/src/main/java/AppConfig.java
@@ -1,16 +1,19 @@
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
-
-import com.pluralsight.repository.CustomerRepository;
-import com.pluralsight.repository.HibernateCustomerRepositoryImpl;
-import com.pluralsight.service.CustomerService;
-import com.pluralsight.service.CustomerServiceImpl;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.context.support.PropertySourcesPlaceholderConfigurer;
 
 @Configuration
 @ComponentScan({"com.pluralsight"})
+@PropertySource("app.properties")
 public class AppConfig {
 
+	@Bean
+	public static PropertySourcesPlaceholderConfigurer getPropertySourcesPlaceholderConfigurer() {
+		return new PropertySourcesPlaceholderConfigurer();
+	}
+	
 	/*
 	@Bean(name="customerService")
 	public CustomerService getCustomerService() {
--- spring_sample_java/src/main/java/com/pluralsight/repository/HibernateCustomerRepositoryImpl.java
+++ spring_sample_java/src/main/java/com/pluralsight/repository/HibernateCustomerRepositoryImpl.java
@@ -3,6 +3,7 @@ package com.pluralsight.repository;
 import java.util.Arrays;
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Repository;
 
 import com.pluralsight.model.Customer;
@@ -10,8 +11,13 @@ import com.pluralsight.model.Customer;
 @Repository("customerRepository")
 public class HibernateCustomerRepositoryImpl implements CustomerRepository {
 	
+	@Value("${dbUsername}")
+	private String dbUsername;
+	
 	@Override
 	public List<Customer> findAll() {
+		System.out.println(this.dbUsername);
+		
 		Customer customer = new Customer();
 		customer.setFirstname("Patrik");
 		customer.setLastname("Tomecz");
